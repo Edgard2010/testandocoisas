c5c9a4774912a41448e390cd9757238ad6b6b582
Edgard Sousa <edgard.sousa@kantaribopemedia.com>
2022-08-30 19:14:00 -0300

    Alteracao no Program.cs

--- testandocoisas/testandocoisas/Program.cs
+++ testandocoisas/testandocoisas/Program.cs
@@ -6,7 +6,7 @@ namespace testandocoisas
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! asasdasdsadsad");
         }
     }
 }
